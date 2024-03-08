02a3908300633724286ac5c4e64eb2d1aa832413
biizakkyzyazem <azemeshimova@gmail.com>
2024-03-08 17:46:47 +0600

    js 3

---
+++ index.js
@@ -0,0 +1,35 @@
+// const iswinner = false;
+
+// if (iswinner){
+//     alert(iswinner);
+//     alert('kuttuktaim! uttun! ');
+// } else{
+//     alert(iswinner);
+//     alert('kapa bolbo! myna saga momposui');
+// }
+// function getString(str) {
+//     if (str === 'hello') {
+//         console.log('hi');
+//     } else if (str === 'bye') {
+//         console.log('bye');
+//     } else if (str < 5) {
+//         console.log('i am higher than 5');
+//     } else if (str === 123) {
+//         console.log('i am string"123');
+//     }
+//     else {
+//         console.log('are you ok');
+//     }
+// }
+
+// getString(123);
+
+// console.log(6>9);
+// console.log(6<9);
+
+// console.log(10!== 10);
+// console.log(10 === 10);
+
+// console.log(5 >= 4);
+
+console.log('Hello' <'hello');
